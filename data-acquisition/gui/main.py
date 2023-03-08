5cdf02b396f30a29ffe4c5d54d08d7456a8434ee
Samy <samynassar996@gmail.com>
2023-03-08 04:23:44 +0200

    Implement flickering box with PyGame
     - Use PyGame instead of pyside6 in flickering box implementation

--- data-acquisition/gui/main.py
+++ data-acquisition/gui/main.py
@@ -1,10 +1,15 @@
 import sys
 
+import pygame
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 
 from widgets.LogoLabel import LogoLabel
 
+
 from widgets.CountdownMessageBox import CountdownMessageBox
+
+from widgets.FlickeringBox import FlickeringManager
 
 
 class MainDataAcquisitionWindow(QWidget):
@@ -19,10 +24,15 @@ class MainDataAcquisitionWindow(QWidget):
         logo_width = 400
         logo_height = 400
         logo_label = LogoLabel(logo_width, logo_height)
+        logo_label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
         # Description Label
-        description_label = QLabel(self)
+        description_label = QLabel()
+        description_label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         description_label.setText("Welcome in data acquisition section")
+
+        # TODO: Implement RadioButton for choosing training with epocing duration or production
+        # TODO: Implement frequencies options
 
         # Starting Button
         starting_button = QPushButton()
@@ -36,14 +46,61 @@ class MainDataAcquisitionWindow(QWidget):
 
         self.setLayout(v_layout)
 
+        # Flickering box initialization
+        self.screen = None
+        self.clock = None
+        self.flickeringManager = None
+        self.done = False
+        self.display = [1024, 768]
+
+
+    def startFlickering(self):
+        pygame.init()
+        pygame.display.set_caption("Flickering Box")
+        self.screen = pygame.display.set_mode(self.display)
+
+        self.done = False
+        self.clock = pygame.time.Clock()
+        self.flickeringManager = FlickeringManager(self.screen)
+
+        self.flickeringManager.add('left', 1)
+        self.flickeringManager.add('top', 5)
+        self.flickeringManager.add('right', 6)
+        self.flickeringManager.add('bottom', 7)
+
+        while not self.done:
+            for event in pygame.event.get():
+                if (event.type == pygame.KEYUP) or (event.type == pygame.KEYDOWN):
+                    if event.key == pygame.K_ESCAPE:
+                        self.done = True
+                if event.type == pygame.QUIT:
+                    self.done = True
+            self.screen.fill((0, 0, 0))
+            self.clock.tick(60)  # 16 ms between frames ~ 60FPS
+            self.flickeringManager.process()
+            self.flickeringManager.draw()
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+
     def start_clicked(self):
-        print("Countdown Started")
-        self.Countdown = CountdownMessageBox()
-        self.Countdown.show()
+        # print("Countdown Started")
+        # self.Countdown = CountdownMessageBox(2)
+        # self.Countdown.show()
+        self.startFlickering()
+
+
+        # main.main()
+        # self.f = FlickeringBox(20, 5)
+        # self.f.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainDataAcquisitionWindow()
     window.show()
+    # window = FlickeringBox(15)
+    # window.show()
     app.exec()
--- data-acquisition/gui/widgets/FlickeringBox.py
+++ data-acquisition/gui/widgets/FlickeringBox.py
@@ -1,60 +1,84 @@
-import sys
-import time
-
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QColor, QBrush, QPainter
-from PySide6.QtWidgets import QWidget, QApplication
+from pygame import Surface, Rect
 
 
-class FlickeringBox(QWidget):
+class Checkerboard:
+    def create(self, a=5, b=5, w=100, h=100):
+        k = w / a
+        l = h / b
 
-    def __init__(self, freq: int, duration: int, fColor: str = 'black', sColor: str = 'white'):
-        super().__init__()
-        self._timer = None
+        white = (255, 255, 255)
+        black = (0, 0, 0)
 
-        self.freq = freq
-        self.duration = duration
-        self.periodicTime = int(1000 / (2 * self.freq))  # The time of displaying one color.
+        surf = Surface((w, h))
+        c = black if self else white
+        surf.fill(c)
 
-        self.firstColor = fColor
-        self.secondColor = sColor
-
-        self.brushes = [QBrush(QColor(self.firstColor)), QBrush(QColor(self.secondColor))]
-        self.currentColorIndex = 0
-        self.counter = 0
-
-        self.t_start = time.time()
-        self.startFlashing()
-
-    def startFlashing(self):
-        self._timer = self.startTimer(self.periodicTime, Qt.PreciseTimer)
-
-    def stopFlashing(self):
-        self.currentColorIndex = 0
-        if self._timer:
-            self.killTimer(self._timer)
-        self._timer = None
-        self.update()
-
-    def timerEvent(self, event):
-        self.counter += 1
-        self.currentColorIndex = (self.currentColorIndex + 1) % 2
-        print(self.counter / 2)
-        print(time.time() - self.t_start)
-        if int(time.time() - self.t_start) == self.duration:
-            print("Duration is ended!")
-            self.stopFlashing()
-            self.close()
-        self.update()
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.fillRect(event.rect(), self.brushes[self.currentColorIndex])
+        for i in range(0, a):
+            for j in range(0, b):
+                c = white if c == black else black
+                r = Rect(k * i, l * j, k, l)
+                surf.fill(c, r)
+        return surf
 
 
-if __name__ == '__main__':
-    app = QApplication([])
-    window = FlickeringBox(4, 2)
-    # window.startFlashing()
-    window.show()
-    sys.exit(app.exec())
+IMAGES = [
+    Checkerboard.create(0),
+    Checkerboard.create(1)
+]
+A = IMAGES[0].get_width()
+
+
+class FlickeringManager:
+    def __init__(self, screen):
+        self.flickies = []
+        self.screen = screen
+
+    def addFlicky(self, f):
+        self.flickies.append(f)
+
+    def add(self, location, frames):
+        w, h = self.screen.get_size()
+        if location == 'left':
+            x = 0
+            y = h / 2 - A / 2
+        elif location == 'right':
+            x = w - A
+            y = h / 2 - A / 2
+        elif location == 'top':
+            y = 0
+            x = w / 2 - A / 2
+        elif location == 'bottom':
+            y = h - A
+            x = w / 2 - A / 2
+        else:
+            raise ValueError("location %s unknown" % location)
+        f = Flicky(x, y, frames)
+        self.flickies.append(f)
+
+    def process(self):
+        for f in self.flickies:
+            f.process()
+
+    def draw(self):
+        for f in self.flickies:
+            f.draw(self.screen)
+
+
+class Flicky(object):
+    def __init__(self, x, y, frames=10, w=A, h=A):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.frames = frames
+        self.clock = 0
+        self.img_index = 0
+
+    def process(self):
+        self.clock = self.clock + 1
+        if self.clock >= self.frames:
+            self.clock = 0
+            self.img_index = 0 if self.img_index == 1 else 1
+
+    def draw(self, screen):
+        screen.blit(IMAGES[self.img_index], (self.x, self.y))
