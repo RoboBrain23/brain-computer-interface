4173e2eef4fa205a15164b6c84369a8c21721431
Samy <samynassar996@gmail.com>
2023-03-15 03:42:08 +0200

    Fix data_acquisition path bug

--- data_acquisition/modules/utils/Logger.py
+++ data_acquisition/modules/utils/Logger.py
@@ -1,14 +1,17 @@
 import logging
+import os
 
 
 def Logger(name):
     # instantiate logger
+    dir_path = os.path.dirname(os.path.realpath("data_acquisition"))
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
     # define handler and formatter
     # handler = logging.StreamHandler()     # This for stdout logging
-    handler = logging.FileHandler(f"{dir}/logs/{name}.log", mode='a')  # This for file logging
+    handler = logging.FileHandler(f"{dir_path}/logs/{name}.log", mode='a')  # This for file logging
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 
     # add formatter to handler
