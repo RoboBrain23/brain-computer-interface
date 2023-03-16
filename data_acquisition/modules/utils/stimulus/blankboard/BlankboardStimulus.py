6e442ecfeec87f9e52ff36047fa3ad1e6f2a1bc0
Samy <samynassar996@gmail.com>
2023-03-16 23:03:08 +0200

    Implement a blankboard stimulus
    Blankboard consists of only one color instead of checkerboards to reduce eye fatigue.

---
+++ data_acquisition/modules/utils/stimulus/blankboard/BlankboardStimulus.py
@@ -0,0 +1,95 @@
+import pygame
+import time
+
+from data_acquisition.config.config import *
+from Box import Box
+
+
+class BlankboardStimulus:
+    def __init__(self, frequencies: dict, preparation_duration: int, stimulation_duration: int,
+                 rest_duration: int, is_full_screen: bool = True):
+        """
+
+        :param frequencies: Dictionary of the boxes frequencies in this order (Top, Right, Down, Left)
+
+        :param preparation_duration: The duration of the Epoc
+
+        :param stimulation_duration: The duration of the Epoc
+
+        :param rest_duration: The duration of the break
+
+        :param is_full_screen: Indicator to run stimulus in a full-screen mode
+
+        """
+        self._done = False
+        self._frequencies = frequencies
+        self.preparation_duration = preparation_duration
+        self.stimulation_duration = stimulation_duration
+        self.rest_duration = rest_duration
+        self.is_full_screen = is_full_screen
+
+        self.screen_width = 1024
+        self.screen_height = 780
+
+        # Create the screen
+        self._screen = pygame.display.set_mode((0, 0),
+                                               pygame.FULLSCREEN) if self.is_full_screen else pygame.display.set_mode(
+            [self.screen_width, self.screen_height])
+
+        self._boxes = []
+        self._create_boxes()
+
+    def _create_boxes(self):
+        """
+        Creates a list of boxes based on the position and frequency data stored in the _frequencies dictionary.
+        :return:
+        """
+        screen_width = self._screen.get_width()
+        screen_height = self._screen.get_height()
+
+        for position, frequency in self._frequencies.items():
+            box = Box(position, frequency, screen_width, screen_height)
+            self._boxes.append(box)
+
+    def run(self):
+        pygame.init()
+
+        # Set the initial time
+        start_time = time.time()
+
+        # Main loop
+        while not self._done:
+            # Get the current time
+            current_time = time.time()
+
+            # Calculate the time elapsed since the last frame
+            delta_time = current_time - start_time
+
+            # Clear the screen
+            self._screen.fill((0, 0, 0))
+
+            # Draw the box if the elapsed time is less than half the period
+            for box in self._boxes:
+                curr_frequency = box.get_frequency()
+                curr_color = box.get_color()
+
+                if delta_time % (1 / curr_frequency) < 1 / (2 * curr_frequency):
+                    pygame.draw.rect(self._screen, curr_color, box.rect(), border_radius=15)
+
+            # Update the screen
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if (event.type == pygame.KEYUP) or (event.type == pygame.KEYDOWN):
+                    if event.key == pygame.K_ESCAPE:
+                        self._done = True
+                if event.type == pygame.QUIT:
+                    self._done = True
+        pygame.quit()
+
+    def close_stimulation(self):
+        self._done = True
+
+
+if __name__ == '__main__':
+    BlankboardStimulus(FREQUENCIES_DICT, PREPARATION_TIME, STIMULATION_TIME, REST_TIME).run()
---
+++ data_acquisition/modules/utils/stimulus/blankboard/Box.py
@@ -0,0 +1,59 @@
+import pygame
+
+from data_acquisition.config.config import *
+
+
+class Box:
+    def __init__(self, position, frequency, screen_width, screen_height):
+        self._frequency = frequency
+        self._position = position
+        self._color = BLUE
+        self._screen_width = screen_width
+        self._screen_height = screen_height
+        self._left = 0
+        self._top = 0
+
+    def get_position(self, position):
+        """
+        get the left and top positions for a rectangle based on the desired position on the screen.
+
+        :param position: The desired position of the rectangle, which can be one of the following constants: LEFT, RIGHT, TOP, DOWN, or CENTER.
+
+        :return: A tuple containing the left and top positions of the rectangle
+        """
+        if position == LEFT_POSITION:
+            left = 0
+            top = self._screen_height / 2 - BOX_HEIGHT / 2
+        elif position == RIGHT_POSITION:
+            left = self._screen_width - BOX_WIDTH
+            top = self._screen_height / 2 - BOX_HEIGHT / 2
+        elif position == TOP_POSITION:
+            top = 0
+            left = self._screen_width / 2 - BOX_WIDTH / 2
+        elif position == DOWN_POSITION:
+            top = self._screen_height - BOX_HEIGHT
+            left = self._screen_width / 2 - BOX_WIDTH / 2
+        elif position == CENTER_POSITION:
+            left = self._screen_width / 2 - BOX_WIDTH / 2
+            top = self._screen_height / 2 - BOX_HEIGHT / 2
+        else:
+            raise ValueError("location %s unknown" % position)
+
+        return left, top
+
+    def get_frequency(self):
+        return self._frequency
+
+    def get_color(self):
+        return self._color
+
+    def rect(self):
+        left, top = self.get_position(self._position)
+        rect = pygame.Rect(left, top, BOX_WIDTH, BOX_HEIGHT)
+        return rect
+
+    def toggle_color(self):
+        """
+        Toggle the color of the box between blue and purple.
+        """
+        self._color = BLUE if self._color == PURPLE else PURPLE
