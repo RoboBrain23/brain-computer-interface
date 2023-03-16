b5fe9981eb03a9049b3c1fc1fdd098fdfd03ed27
Samy <samynassar996@gmail.com>
2023-03-16 23:07:49 +0200

    Make logger use stdout and file handler at the same time.

--- data_acquisition/modules/utils/Logger.py
+++ data_acquisition/modules/utils/Logger.py
@@ -10,14 +10,20 @@ def Logger(name):
     logger.setLevel(logging.INFO)
 
     # define handler and formatter
-    # handler = logging.StreamHandler()     # This for stdout logging
-    handler = logging.FileHandler(f"{dir_path}/logs/{name}.log", mode='a')  # This for file logging
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    stdout_handler = logging.StreamHandler()  # This for stdout logging
+    file_handler = logging.FileHandler(f"{dir_path}/logs/{name}.log", mode='a')  # This for file logging
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)2d - %(message)s | %(threadName)s:%(thread)d")
 
     # add formatter to handler
-    handler.setFormatter(formatter)
+    stdout_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
-    # add handler to logger
-    logger.addHandler(handler)
+    if not logger.hasHandlers():
+        logger.addHandler(stdout_handler)
+        logger.addHandler(file_handler)
 
     return logger
+
+
+app_logger = Logger("app")
