4c5f5141b899251ed94a372a939cfc30482a040f
Samy <samynassar996@gmail.com>
2023-03-26 03:34:44 +0200

    Fix the random direction list

--- data_acquisition/main.py
+++ data_acquisition/main.py
@@ -3,7 +3,7 @@ import random
 from data_acquisition.config.config import *
 from data_acquisition.modules.ssvep import SSVEP
 
-subject_name = ""
+subject_name = "Samy"
 no_of_sessions = 5
 preparation_duration = 2
 stimulation_duration = 5
@@ -13,7 +13,8 @@ full_screen = True
 base_directions = [TOP_POSITION, RIGHT_POSITION, DOWN_POSITION, LEFT_POSITION]
 final_order = []
 for i in range(no_of_sessions):
-    final_order.append(random.shuffle(base_directions))
+    random.shuffle(base_directions)
+    final_order.append(list(base_directions))
 
 
 ssvep = SSVEP(subject_name, preparation_duration, stimulation_duration, rest_duration, FREQUENCIES_DICT, full_screen, final_order)
