c64fa09407783e7f749087f5693e534468535676
Samy <samynassar996@gmail.com>
2023-04-09 23:37:16 +0200

    Remove sensors quality and battery implementation

--- data_acquisition/modules/headset/EEG.py
+++ data_acquisition/modules/headset/EEG.py
@@ -19,72 +19,6 @@ logger = app_logger  # Log all in app.log
 
 tasks = queue.Queue()
 
-sensor_bits = {
-    'F3': [10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7],
-    'FC5': [28, 29, 30, 31, 16, 17, 18, 19, 20, 21, 22, 23, 8, 9],
-    'AF3': [46, 47, 32, 33, 34, 35, 36, 37, 38, 39, 24, 25, 26, 27],
-    'F7': [48, 49, 50, 51, 52, 53, 54, 55, 40, 41, 42, 43, 44, 45],
-    'T7': [66, 67, 68, 69, 70, 71, 56, 57, 58, 59, 60, 61, 62, 63],
-    'P7': [84, 85, 86, 87, 72, 73, 74, 75, 76, 77, 78, 79, 64, 65],
-    'O1': [102, 103, 88, 89, 90, 91, 92, 93, 94, 95, 80, 81, 82, 83],
-    'O2': [140, 141, 142, 143, 128, 129, 130, 131, 132, 133, 134, 135, 120, 121],
-    'P8': [158, 159, 144, 145, 146, 147, 148, 149, 150, 151, 136, 137, 138, 139],
-    'T8': [160, 161, 162, 163, 164, 165, 166, 167, 152, 153, 154, 155, 156, 157],
-    'F8': [178, 179, 180, 181, 182, 183, 168, 169, 170, 171, 172, 173, 174, 175],
-    'AF4': [196, 197, 198, 199, 184, 185, 186, 187, 188, 189, 190, 191, 176, 177],
-    'FC6': [214, 215, 200, 201, 202, 203, 204, 205, 206, 207, 192, 193, 194, 195],
-    'F4': [216, 217, 218, 219, 220, 221, 222, 223, 208, 209, 210, 211, 212, 213]
-}
-quality_bits = [99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112]
-
-battery_values = {
-    "255": 100,
-    "254": 100,
-    "253": 100,
-    "252": 100,
-    "251": 100,
-    "250": 100,
-    "249": 100,
-    "248": 100,
-    "247": 99,
-    "246": 97,
-    "245": 93,
-    "244": 89,
-    "243": 85,
-    "242": 82,
-    "241": 77,
-    "240": 72,
-    "239": 66,
-    "238": 62,
-    "237": 55,
-    "236": 46,
-    "235": 32,
-    "234": 20,
-    "233": 12,
-    "232": 6,
-    "231": 4,
-    "230": 3,
-    "229": 2,
-    "228": 2,
-    "227": 2,
-    "226": 1,
-    "225": 0,
-    "224": 0,
-}
-
-g_battery = 0
-
-def get_level(data, bits):
-    """
-    Returns sensor level value from data using sensor bit mask in micro volts (uV).
-    """
-    level = 0
-    for i in range(13, -1, -1):
-        level <<= 1
-        b, o = (bits[i] / 8) + 1, bits[i] % 8
-        level |= (ord(data[b]) >> o) & 1
-    return level
-
 
 class EEG(object):
 
@@ -95,7 +29,6 @@ class EEG(object):
         self.delimiter = ", "
         self.key = ""
         self.cipher = None
-
 
         devices_used = 0
 
@@ -119,8 +52,6 @@ class EEG(object):
             logger.error("Can't find EPOC+ usb")
             os._exit(0)
         # sn = self.serial_number
-
-
 
     def _data_handler(self, data):
         join_data = ''.join(map(chr, data[1:]))
@@ -202,57 +133,6 @@ class EEG(object):
         except Exception as e:
             print("Recording err:")
             print(e)
-
-    def handle_quality(self, sensors):
-        """
-        Sets the quality value for the sensor from the quality bits in the packet data.
-        Optionally will return the value.
-
-        :param sensors - reference to sensors dict in Emotiv class.
-
-        """
-        if self.old_model:
-            current_contact_quality = get_level(self.raw_data, quality_bits) / 540
-        else:
-            current_contact_quality = get_level(self.raw_data, quality_bits) / 1024
-        sensor = ord(self.raw_data[0])
-        if sensor == 0 or sensor == 64:
-            sensors['F3']['quality'] = current_contact_quality
-        elif sensor == 1 or sensor == 65:
-            sensors['FC5']['quality'] = current_contact_quality
-        elif sensor == 2 or sensor == 66:
-            sensors['AF3']['quality'] = current_contact_quality
-        elif sensor == 3 or sensor == 67:
-            sensors['F7']['quality'] = current_contact_quality
-        elif sensor == 4 or sensor == 68:
-            sensors['T7']['quality'] = current_contact_quality
-        elif sensor == 5 or sensor == 69:
-            sensors['P7']['quality'] = current_contact_quality
-        elif sensor == 6 or sensor == 70:
-            sensors['O1']['quality'] = current_contact_quality
-        elif sensor == 7 or sensor == 71:
-            sensors['O2']['quality'] = current_contact_quality
-        elif sensor == 8 or sensor == 72:
-            sensors['P8']['quality'] = current_contact_quality
-        elif sensor == 9 or sensor == 73:
-            sensors['T8']['quality'] = current_contact_quality
-        elif sensor == 10 or sensor == 74:
-            sensors['F8']['quality'] = current_contact_quality
-        elif sensor == 11 or sensor == 75:
-            sensors['AF4']['quality'] = current_contact_quality
-        elif sensor == 12 or sensor == 76 or sensor == 80:
-            sensors['FC6']['quality'] = current_contact_quality
-        elif sensor == 13 or sensor == 77:
-            sensors['F4']['quality'] = current_contact_quality
-        elif sensor == 14 or sensor == 78:
-            sensors['F8']['quality'] = current_contact_quality
-        elif sensor == 15 or sensor == 79:
-            sensors['AF4']['quality'] = current_contact_quality
-        else:
-            sensors['Unknown']['quality'] = current_contact_quality
-            sensors['Unknown']['value'] = sensor
-        return current_contact_quality
-
 
     def start_recording(self, csv_data_file: str, csv_meta_data_file: str, preparation_duration: int,
                         stimulation_duration: int, rest_duration: int, frequencies: dict, directions_order: list):
@@ -353,23 +233,14 @@ class EEG(object):
                 "The recording session is done after {} seconds and saved at :".format(time.time() - starting_time))
             logger.info("File Path>> " + csv_data_file)
 
+
 if __name__ == '__main__':
     headset = EEG()
     while 1:
         data = tasks.get()
-        g_battery = 0
 
-        counter = data[0]
-        # counter = ord(c)
-        if counter > 127:
-            g_battery = battery_values[str(counter)]
-            counter = 128
-
-        d=''
-
+        d = ''
         for i in range(len(data)):
             d += f"{data[i]}, "
-        print(f"battery: {data[16]}")
-        print(f"G_battery: {g_battery}")
 
         print(d)
