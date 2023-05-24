90dfc84f482de7756aa91c44db7c74f258f294e6
ahmed hamada <ahmedhamada507@gmail.com>
2023-05-24 21:40:15 +0300

    Add train,test and train_validation split functions

--- models/nn_base.py
+++ models/nn_base.py
@@ -1,5 +1,9 @@
 import numpy as np
 from keras.utils import to_categorical
+from keras.callbacks import ReduceLROnPlateau, CSVLogger, TensorBoard, ModelCheckpoint
+from keras.optimizers import Adam
+from keras.layers import Input
+from keras.models import Model, load_model
 
 
 class NNBase:
@@ -58,6 +62,76 @@ class NNBase:
             y_train[i] = to_categorical(y_data, self.num_classes)
 
         return x_train, y_train
+
+    def __train_val_split(self, y_label):
+        train_list = np.arange(y_label.shape[0])
+        val_list = np.random.choice(train_list, size=y_label.shape[0] // 10, replace=False)
+        train_list = np.delete(train_list, val_list)
+        return train_list, val_list
+
+    def train(self, train_data, y_label, start_time, batch_size=32, epochs=100, save_path='model'):
+        win_trian = int(self.window_time * self.fs)
+        train_list, val_list = self.__train_val_split(y_label)
+        train_generator = self.__train_data_generator(batch_size, train_data, win_trian, y_label, start_time,
+                                                      train_list, train_data.shape[0])
+        val_generator = self.__train_data_generator(batch_size, train_data, win_trian, y_label, start_time,
+                                                    val_list, train_data.shape[0])
+        input_tensor = Input(shape=self.input_shape)
+        preds = self.model(input_tensor)
+        model = Model(input_tensor, preds)
+        # model.summary()
+        # model_name = 'tCnn_model_'+str(t_train)+'s.h5'
+        model_name = save_path + '_tCNN_' + str(self.window_time) + 's.{epoch:02d}-{val_accuracy:.4f}.h5'
+        model_checkpoint = ModelCheckpoint(model_name, monitor='val_loss', verbose=1, save_best_only=True,
+                                           mode='min')
+        adam = Adam(learning_rate=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=15, min_lr=0.0003)
+        csv_logger = CSVLogger(save_path + "TCNN" + ".log")
+        tensorboard = TensorBoard(log_dir=save_path + "TCNN_tb" + ".log", histogram_freq=1, write_graph=True,
+                                  write_images=True)
+        model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+        # print(model.summary())
+        history = model.fit(
+            train_generator,
+            steps_per_epoch=10,
+            epochs=epochs,
+            validation_data=val_generator,
+            validation_steps=1,
+            callbacks=[model_checkpoint, reduce_lr]
+        )
+
+    def test(self, test_data, y_label, start_time, batch_size=32, fs=128, model_path='model'):
+        win_train = int(self.window_time * fs)
+        if model_path == 'model':
+            model_name = model_path + '_tCNN_' + str(self.window_time) + 's.{epoch:02d}-{val_accuracy:.4f}.h5'
+        else:
+            model_name = model_path
+        model = load_model(model_name)
+        print('load model: ', model_name)
+        acc_list = []
+        av_acc_list = []
+        for i in range(5):
+            x_train, y_train = self.__test_data_generator(batch_size, test_data, win_trian, y_label, start_time,
+                                                          test_data.shape[0])
+            a, b = 0, 0
+            # get the predicted results of the batchsize test samples
+            y_pred = model.predict(np.array(x_train))
+            true, pred = [], []
+            y_true = y_train
+            # Calculating the accuracy of current time
+            for i in range(batch_size - 1):
+                y_pred_ = np.argmax(y_pred[i])
+                pred.append(y_pred_)
+                y_true1 = np.argmax(y_train[i])
+                true.append(y_true1)
+                if y_true1 == y_pred_:
+                    a += 1
+                else:
+                    b += 1
+            acc = a / (a + b)
+            acc_list.append(acc)
+        av_acc = np.mean(acc_list)
+        print(av_acc)
 
     def set_input_shape(self, input_shape):
         self.input_shape = input_shape
