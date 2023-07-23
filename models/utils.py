6717cfacffa5cc56f30fa6d3b5f85684f8120442
ahmed hamada <ahmedhamada507@gmail.com>
2023-07-23 18:06:21 +0300

    Implement Filters
    - Implement bandpass filter.
    - Implement filterbank.
    - Implement Harmonic freq.
    - Add filters to generators.

--- models/utils.py
+++ models/utils.py
@@ -2,6 +2,61 @@ import numpy as np
 from keras.utils import to_categorical
 import os
 
+def filter_data(data, low=9, high=15, fs=128, order=4,is_fb=False):
+    """
+    filter the data with the bandpass filter :
+
+    if is_fb is True, the filter bank will be used, the data will be filtered
+    by the bandpass filter in the frequency range of each harmonics of the low frequency and the high frequency.
+
+    if is_fb is False, the data will be filtered by the bandpass filter
+    in the frequency range of first harmonics of the low frequency and the high frequency.
+
+    :param data: the data to be filtered
+
+    :param low: the lowest frequency of the our target frequencies
+
+    :param high: the highest frequency of the our target frequencies
+
+    :param fs: the sampling frequency
+
+    :param order: the order of the filter
+
+    :return: the filtered data
+    """
+    harmonics = get_harmonics(low, high)
+    if is_fb:
+        filtered_data = []
+        for freq_range in harmonics:
+            wn1, wn2 = 2 * freq_range[0] / fs, 2 * freq_range[1] / fs
+            channel_data_list = []
+            for i in range(data.shape[1]):
+                b, a = butter(order, [wn1, wn2], 'bandpass')
+                filtedData = filtfilt(b, a, data[:, i])
+                channel_data_list.append(filtedData)
+            filtered_data.append(np.array(channel_data_list))
+        return np.array(filtered_data)
+    else:
+        wn1 = 2*harmonics[0][0]/fs
+        wn2 = 2*harmonics[-1][-1]/fs
+        channel_data_list = []
+        for i in range(data.shape[1]):
+            b, a = butter(order, [wn1,wn2], 'bandpass')
+            filtedData = filtfilt(b, a, data[:,i])
+            channel_data_list.append(filtedData)
+        channel_data_list = np.array([channel_data_list])
+
+        return channel_data_list
+    
+
+def get_harmonics(start_freq, end_freq):
+    harmonics = []
+    # i = 1
+    for i in range(1,4):
+        harmonics.append([int(i * start_freq) - 2, int(i * end_freq) + 2])
+        # i += 1
+    return harmonics
+
 
 def train_val_split(y_label):
     """
@@ -17,7 +72,7 @@ def train_val_split(y_label):
     return train_list, val_list
 
 
-def train_data_generator(batch_size, data, y_label, start_time, train_list, num_classes, input_shape):
+def train_data_generator(batch_size, data,  y_label, start_time, train_list,num_classes,input_shape,is_fb=False):
     """
     generate the training data
 
@@ -37,64 +92,73 @@ def train_data_generator(batch_size, data, y_label, start_time, train_list, num_
 
     :return:  the training data
     """
-    input_shape = (batch_size,) + input_shape
-    x_train = np.zeros(input_shape, dtype=np.float32)
+    input_shape = (batch_size,)+input_shape
+    if input_shape[-1] != 1:
+        input_shape = input_shape[:-1] + (1,)
+    x_train1 = np.zeros(input_shape, dtype=np.float32)
+    if is_fb:
+        x_train2 = np.zeros(input_shape, dtype=np.float32)
+        x_train3 = np.zeros(input_shape, dtype=np.float32)
+    # x_train4 = np.zeros((batch_size, self.channel, win_train, 1), dtype=np.float32)
     y_train = np.zeros((batch_size, num_classes), dtype=np.float32)
     while True:
-        labels_count = np.zeros(num_classes)
-        i = 0
-        while np.sum(labels_count) < batch_size:
+        # get training samples of batch_size trials
+        for i in range(batch_size):
+            # randomly selecting the single-trial
             k = np.random.choice(train_list)
+            # get the label of the single-trial
             y_data = y_label[k]
-            if labels_count[y_data] < batch_size // num_classes:
-                labels_count[y_data] += 1
-            else:
-                continue
-            time_start = np.random.randint(0, int(630 - input_shape[2])) - start_time[0]
+            # randomly selecting a single-sample in the single-trial, 35 is the frames of the delay-time, 450 frames is the data range we used
+            time_start = np.random.randint(0,int(600 - input_shape[2]))
             x1 = int(start_time[k]) + time_start
             x2 = int(start_time[k]) + time_start + input_shape[2]
-            x_train_data = data[x1:x2, :]
-            # todo: add preprocessing functions here
-
-            x_train[i] = np.reshape(x_train_data[0][:, x1:x2], input_shape[1:])
+            x_2 = np.reshape(data[0][:, x1:x2], input_shape[1:])
+            x_train1[i] = x_2                    
+            y_train[i] = to_categorical(y_data, num_classes=4, dtype='float32')
+            x_train1[i] = np.reshape(data[0][:, x1:x2], input_shape[1:])
+            if is_fb:
+                x_train2[i] = np.reshape(data[1][:, x1:x2], input_shape[1:])
+                x_train3[i] = np.reshape(data[2][:, x1:x2], input_shape[1:])
+            # x_train4[i] = np.reshape(train_data4[:, x1:x2], (self.channel, win_train, 1))
             y_train[i] = to_categorical(y_data, num_classes=num_classes, dtype='float32')
-            i += 1
-        yield x_train, y_train
+        
+        if is_fb:
+            x_train = np.concatenate((x_train1, x_train2, x_train3), axis=-1)
+            yield x_train, y_train
+        else:
+            yield x_train1, y_train
 
 
-def test_data_generator(batch_size, data, y_label, start_time, num_classes, input_shape):
-    """
-    generate the test data
 
-    :param batch_size:  the size of the batch
+def test_data_generator(batch_size, data, y_label, start_time,num_classes,input_shape,is_fb=False):
+    input_shape = (batch_size,)+input_shape
+    if input_shape[-1] != 1:
+        input_shape = input_shape[:-1] + (1,)
 
-    :param data: the data of the test data
-
-    :param y_label:  the label of the test data
-
-    :param start_time:  the start time of the test data
-
-    :param num_classes:  the number of the classes
-
-    :param input_shape:  the shape of the input data
-
-    :return:  the test data
-    """
-    input_shape = (batch_size,) + input_shape
-    x_train = np.zeros(input_shape, dtype=np.float32)
+    x_train1 = np.zeros(input_shape, dtype=np.float32)
+    if is_fb:
+        x_train2 = np.zeros(input_shape, dtype=np.float32)
+        x_train3 = np.zeros(input_shape, dtype=np.float32)
+    # x_train4 = np.zeros((batch_size, self.channel, win_train, 1), dtype=np.float32)
     y_train = np.zeros((batch_size, num_classes), dtype=np.float32)
     for i in range(batch_size):
         k = np.random.randint(0, y_label.shape[0])
         y_data = y_label[k]
-        time_start = np.random.randint(0, int(630 - input_shape[2])) - start_time[0]
+        time_start = np.random.randint(0, int(1000 - input_shape[2])) - start_time[0]
         x1 = int(start_time[k]) + time_start
         x2 = int(start_time[k]) + time_start + input_shape[2]
-        x_train_data = data[x1:x2, :]
-        # todo: add preprocessing functions here
-
-        x_train[i] = np.reshape(x_train_data[0][:, x1:x2], input_shape[1:])
+        x_train1[i] = np.reshape(data[0][:, x1:x2], input_shape[1:])
+        if is_fb:
+            x_train2[i] = np.reshape(data[1][:, x1:x2], input_shape[1:])
+            x_train3[i] = np.reshape(data[2][:, x1:x2], input_shape[1:])
+        # x_train4[i] = np.reshape(train_data4[:, x1:x2], (self.channel, win_train, 1))
         y_train[i] = to_categorical(y_data, num_classes=num_classes, dtype='float32')
-    return x_train, y_train
+    
+    if is_fb:
+        x_train = np.concatenate((x_train1, x_train2, x_train3), axis=-1)
+        return x_train, y_train
+    else:
+        return x_train1, y_train
 
 
 def save_model(model, model_name, save_path=''):
